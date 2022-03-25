f698608dac647eca7363f7e5fb09a289b8afdb54
Minh <54742686+MinhYz@users.noreply.github.com>
2022-03-25 19:36:54 +0700

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -6,9 +6,16 @@ from bot import dispatcher
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
 
-@loader.tds
+def speedtest(self, servers):
+    test = Speedtest()
+    test = speedtest.Speedtest()
+    test.get_servers(servers)
+    test.get_best_server()
+    test.download(threads=None)
+    test.upload(threads=None)
+    return test.results.dict()
+    
 class SpeedtestMod(loader.Module):
-    """Uses speedtest.net"""
     strings = {"name": "Speedtest",
                "running": "<b>Running speedtest...</b>",
                "results": "<b>Speedtest Results:</b>",
@@ -16,32 +23,22 @@ class SpeedtestMod(loader.Module):
                "results_upload": "<b>Upload:</b> <code>{}</code> <b>MiB/s</b>",
                "results_ping": "<b>Ping:</b> <code>{}</code> <b>ms</b>"}
 
-    async def speedtestcmd(self, message):
-        """Tests your internet speed"""
-        await utils.answer(message, self.strings("running", message))
-        args = utils.get_args(message)
-        servers = []
-        for server in args:
-            try:
-                servers += [int(server)]
+async def speedtestcmd(self, message):
+    await utils.answer(message, self.strings("running", message))
+    args = utils.get_args(message)
+    servers = []\
+    for server in args:
+        try:
+            servers += [int(server)]
             except ValueError:
                 logger.warning("server failed")
-        results = await utils.run_sync(self.speedtest, servers)
-        ret = self.strings("results", message) + "\n\n"
-        ret += self.strings("results_download", message).format(round(results["download"] / 2**20, 2)) + "\n"
-        ret += self.strings("results_upload", message).format(round(results["upload"] / 2**20, 2)) + "\n"
-        ret += self.strings("results_ping", message).format(round(results["ping"], 2)) + "\n"
-        await utils.answer(message, ret)
+                results = await utils.run_sync(self.speedtest, servers)
+                ret = self.strings("results", message) + "\n\n"
+                ret += self.strings("results_download", message).format(round(results["download"] / 2**20, 2)) + "\n"
+                ret += self.strings("results_upload", message).format(round(results["upload"] / 2**20, 2)) + "\n"
+                ret += self.strings("results_ping", message).format(round(results["ping"], 2)) + "\n"
+                await utils.answer(message, ret)
 
-    def speedtest(self, servers):
-        speedtester = speedtest.Speedtest()
-        speedtester.get_servers(servers)
-        speedtester.get_best_server()
-        speedtester.download(threads=None)
-        speedtester.upload(threads=None)
-        return speedtester.results.dict()
-
-SPEED_HANDLER = CommandHandler(BotCommands.SpeedCommand, speedtest,
-                                                  filters=CustomFilters.owner_filter | CustomFilters.authorized_user, run_async=True)
+SPEED_HANDLER = CommandHandler(BotCommands.SpeedCommand, speedtest, filters=CustomFilters.owner_filter | CustomFilters.authorized_user, run_async=True)
 
 dispatcher.add_handler(SPEED_HANDLER)
